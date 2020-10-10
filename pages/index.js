fe89ca77bb76b066dac0a169fee4ceb0d4722473
Valentin <primarh111@gmail.ru>
2020-10-10 09:20:08 +0300

    breadcrambs

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-import Head from 'next/head'
-import Header from '../components/header/Header'
-import styles from '../styles/Home.module.sass'
-
+import Head from "next/head";
+import Header from "../components/header/Header";
+import styles from "../styles/Home.module.sass";
+import Typography from "@material-ui/core/Typography";
+import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import Link from "@material-ui/core/Link";
+import {red} from "@material-ui/core/colors";
 
 export default function Home() {
   return (
@@ -9,13 +12,28 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
 
       <main className={styles.main}>
         <div className={styles.container}>
-          <Header/>
+          <Header />
+          <h2>ЛИЧНЫЙ ПРОФИЛЬ</h2>
+          <Breadcrumbs color="red" aria-label="breadcrumb">
+            <Link  href="/">
+            Главная
+            </Link>
+            <Typography color="palette.text.primary">Личный профиль</Typography>
+          </Breadcrumbs>
         </div>
-      </main> 
+      </main>
     </div>
-  )
+  );
 }
